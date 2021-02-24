d779bb685f0db50edc9f4a879b4103a048e1266e
Shre232 <shreyasgaikwad232.1997@gmail.com>
2021-02-24 19:07:07 +0530

    git

--- src/main/java/stringCalculator.java
+++ src/main/java/stringCalculator.java
@@ -2,15 +2,25 @@
 public class stringCalculator {
         public static int Add(String input){
             String[] numbers=input.split(",");
-            if(input.isEmpty()){
+            if(isEmpty(input)){
                 return 0;
             }
             if (input.length()==1){
                 return stringToInt(input);
             }
             else{
-                return Integer.parseInt(numbers[0])+Integer.parseInt(numbers[1]);
+                return getsum(numbers[0],numbers[1]) ;
             }
+        }
+
+        private static boolean isEmpty(String input){
+            return input.isEmpty();
+
+    }
+
+
+        private static int getsum(String num1,String num2){
+            return Integer.parseInt(num1)+Integer.parseInt (num2);
         }
         private static int stringToInt(String input) {
 
